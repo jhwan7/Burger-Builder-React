80aeed61a63742cf1da6fb6552923a7222cacf79
jhwan7 <jhwan7@uwo.ca>
2020-01-20 19:59:59 -0500

    Outputting burger ingredients dynamically

--- burger_builder/src/components/Burger/Burger.js
+++ burger_builder/src/components/Burger/Burger.js
@@ -6,18 +6,22 @@ import styles from './Burger.module.css'
 
 
 const burger = (props) => {
+    console.log('Object Recieved from props', props.ingredients);
+    const incomingIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            console.log('String index of object recieved:', igKey);
+            console.log('Data (int) stored at the string index', props.ingredients[igKey]);
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                // return ingredients that match igKey
+                console.log(_);
+                console.log('Type: ', igKey, 'Quantity: ', i);
+                return <Ingredient key = {igKey + i} type = {igKey}/>;
+            }) 
+        });
     return (
         <div className = {styles.Burger}>
             <Ingredient type = "bread-top"/>
-            <Ingredient type = "cheese"/>
-            <Ingredient type = "meat"/>
-            <Ingredient type = "meat"/>
-            <Ingredient type = "meat"/>
-            <Ingredient type = "salad"/>
-            <Ingredient type = "salad"/>
-            <Ingredient type = "meat"/>
-            <Ingredient type = "bacon"/>
-            <Ingredient type = "meat"/>
+            {incomingIngredients}
             <Ingredient type = "bread-bottom"/>
         </div>
     );
