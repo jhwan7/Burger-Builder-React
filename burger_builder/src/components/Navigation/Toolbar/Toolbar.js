47da4bbd3f19c83bbf39eb989bbb34c657dea74b
jhwan7 <jhwan7@uwo.ca>
2020-01-25 22:56:04 -0500

    Modify Side Drawer design by adjusting NavigationItems CSS style. (Make more responsive web by adding media queries)

--- burger_builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger_builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -12,7 +12,7 @@ const toolbar = (props) => (
             <Logo/>
         </div>
             
-        <nav>
+        <nav className = {styles.DesktopOnly}>
            <NavigationItems/>
         </nav>
     </header>
